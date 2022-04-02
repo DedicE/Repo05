77f5a354f519d9947fbeb9ea635b7c4a3056a123
Emir Dedic <dedic.emir17@gmail.com>
2022-04-02 19:59:45 +0200

    Fixes

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // Only change code below this line
-    var myName = "Emir";
+    var myName = "Emire";
     var myString = "My name is " + myName + " and I am well!"; 
     // Only change code above this line
     return myString;
